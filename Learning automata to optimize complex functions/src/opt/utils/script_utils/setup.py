f4f79870e9f620a31130cc2b09a586d4f8ceb714
bbraunsfeld <58336625+bbraunsfeld@users.noreply.github.com>
2020-02-28 19:22:28 +0100

    Create setup.py

---
+++ Learning automata to optimize complex functions/src/opt/utils/script_utils/setup.py
@@ -0,0 +1,57 @@
+import os
+import logging
+from shutil import rmtree
+
+__all__ = ["setup_run"]
+
+
+def setup_run(args):
+    argparse_dict = vars(args)
+    jsonpath = os.path.join(args.path, "args.json")
+    if args.mode == "single":
+
+        # build dir
+        if args.overwrite and os.path.exists(args.path):
+            logging.info("existing file will be overwritten...")
+            rmtree(args.path)
+        if not os.path.exists(args.path):
+            os.makedirs(args.path)
+
+        # store single arguments
+        opt.utils.opt_utils.to_json(jsonpath, argparse_dict)
+
+        single_args = args
+    else:
+        # check if path is valid
+        if not os.path.exists(args.path):
+            raise opt.utils.ScriptError(
+                "The selected dir does not exist " "at {}!".format(args.path)
+            )
+
+        # load  arguments
+        single_args = opt.utils.opt_utils.read_from_json(jsonpath)
+        
+    if args.mode == "multi":
+
+        # build dir
+        if args.overwrite and os.path.exists(args.path):
+            logging.info("existing file will be overwritten...")
+            rmtree(args.path)
+        if not os.path.exists(args.path):
+            os.makedirs(args.path)
+
+        # store multi arguments
+        opt.utils.opt_utils.to_json(jsonpath, argparse_dict)
+
+        multi_args = args
+    else:
+        # check if path is valid
+        if not os.path.exists(args.path):
+            raise opt.utils.ScriptError(
+                "The selected dir does not exist " "at {}!".format(args.path)
+            )
+
+        # load arguments
+        multi_args = opt.utils.opt_utils.read_from_json(jsonpath)
+
+    return single_args, multi_args
