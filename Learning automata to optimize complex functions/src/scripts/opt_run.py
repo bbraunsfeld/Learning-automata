4715e7b6603bfdd40dfb1f1e107be33b388c5b92
bbraunsfeld <58336625+bbraunsfeld@users.noreply.github.com>
2020-02-29 19:33:55 +0100

    Update opt_run.py

--- Learning automata to optimize complex functions/src/scripts/opt_run.py
+++ Learning automata to optimize complex functions/src/scripts/opt_run.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 import logging
-from opt.utils import (get_function,get_parameter,ScriptError,setup_run)
+from opt.utils import (get_function,get_parameter,ScriptError,setup_run,picture_results)
 from opt.utils.script_utils.parsing import build_parser
 from opt.algorithms.maximum import *
 
@@ -10,40 +10,34 @@ logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 def main(args):
     # setup
     mode_args = setup_run(args)
-    
+    logging.info("Steup...")
     #get function
     func=get_function(mode_args)
+    print (func)
     
     #get parameter 
-    a, b, r, eps, delt, lamb = get_parameter(args)
+    a, b, r, eps, delt, lamb = get_parameter(mode_args)
+
+    print (a, b, r, eps, delt, lamb)
     
     #single or multi
     if args.mode == "single":
         if args.model == "max":
-            logging.info("searching...")
+            logging.info("Searching for maximum...")
             maximum=find_maximum(func,a,b,r,eps,delt,lamb)
-            logging.info("maximum found...")
+            logging.info("Maximum found at x = %s" %(maximum))
+
+            print ("Maximum at", maximum)
+            picture_result(func,a,b,maximum,args)
             
-            xs=np.arange(a,b,0.1)
-            plt.figure(figsize=(15,3))
-            plt.plot(xs,list(map(func,xs)))
-            plt.plot(xs, np.zeros(xs.shape),color='black',linewidth=1)
-            plt.plot(maximum,0,'.',marker="*",color='red')
-            np.cos(x/4)*(np.cos(3*x)+np.sin(6*x))
-            plt.savefig(args.path)
-            
+  
         elif args.model == "min":
-            logging.info("searching...")
+            logging.info("Searching for minimum...")
             minimum=find_minimum(func,a,b,r,eps,delt,lamb)
-            logging.info("minimum found...")
+            logging.info("Minimum found at x = %s" %(minimum))
             
-            xs=np.arange(a,b,0.1)
-            plt.figure(figsize=(15,3))
-            plt.plot(xs,list(map(func,xs)))
-            plt.plot(xs, np.zeros(xs.shape),color='black',linewidth=1)
-            plt.plot(maximum,0,'.',marker="*",color='red')
-            np.cos(x/4)*(np.cos(3*x)+np.sin(6*x))
-            plt.savefig(args.path)
+            print ("Minimum at x = ", maximum)
+            picture_result(func,a,b,maximum,args)
             
         else:
             raise ScriptError("Unknown mode: {}".format(args.model))
@@ -57,7 +51,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     if args.mode == "from_json":
-        args = opt.utils.read_from_json(args.json_path)
+        args = read_from_json(args.json_path)
 
     import argparse
 
