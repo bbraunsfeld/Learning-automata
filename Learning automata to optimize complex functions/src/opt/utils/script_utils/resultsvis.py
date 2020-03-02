d6da8c9c3ec22763d1af2135550a0e0085dbef7f
bbraunsfeld <58336625+bbraunsfeld@users.noreply.github.com>
2020-03-02 09:08:18 +0100

    Update resultsvis.py

--- Learning automata to optimize complex functions/src/opt/utils/script_utils/resultsvis.py
+++ Learning automata to optimize complex functions/src/opt/utils/script_utils/resultsvis.py
@@ -3,11 +3,14 @@ import matplotlib.pyplot as plt
 from numpy import random 
 
 
-def picture_result(f,a,b,maximum,args):
+def picture_result(optim,f,a,b,optimum,args):
     xs=np.arange(a,b,0.1)
     plt.figure(figsize=(15,3))
     plt.plot(xs,list(map(f,xs)))
     plt.plot(xs, np.zeros(xs.shape),color='black',linewidth=1)
-    plt.plot(maximum,0,'.',marker="*",color='red')
-    plt.axvline(x=maximum,ls='--',color='red')
-    plt.savefig(args.path + "/maximum.eps")
+    plt.plot(optimum,0,'.',marker="*",color='red')
+    plt.axvline(x=optimum,ls='--',color='red')
+    if optim == 'max':
+        plt.savefig(args.path + "/maximum.eps")
+    else optim == 'min':
+        plt.savefig(args.path + "/minimum.eps")
